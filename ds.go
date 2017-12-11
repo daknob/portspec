b550a0a168b40d5825cb4181fb145870bf09b31b
DaKnOb <daknob.mac@gmail.com>
2017-12-11 14:48:46 +0200

    Add code to match new configuration file format

--- ds.go
+++ ds.go
@@ -9,4 +9,11 @@ type config struct {
 	ParallelScans int              `yaml:"parallelscans"`
 	Hosts         map[string][]int `yaml:"hosts"`
 	ScanPorts     []int            `yaml:"scanports"`
+	SendEmail     bool             `yaml:"sendemail"`
+	SMTPServer    string           `yaml:"smtpserver"`
+	SMTPPort      int              `yaml:"smtpport"`
+	SMTPUsername  string           `yaml:"smtpusername"`
+	SMTPPassword  string           `yaml:"smtppassword"`
+	AlertEmail    string           `yaml:"alertemail"`
+	FromEmail     string           `yaml:"fromemail"`
 }
