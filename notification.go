ab7bcf55e53eb119a66433e2ddc4c090f05dbf63
DaKnOb <daknob.mac@gmail.com>
2017-12-18 11:21:41 +0200

    Downgrade Error to Warning in case of mismatch

--- notification.go
+++ notification.go
@@ -13,7 +13,7 @@ notifyForHost sends a notification that a particular host's open ports
 do not match the expected ones specified in the configuration file.
 */
 func notifyForHost(host string, desiredPorts, openPorts []int) {
-	log.Errorf("Host %s port mismatch! Expected: %v. Got: %v.", host, desiredPorts, openPorts)
+	log.Warnf("Host %s port mismatch! Expected: %v. Got: %v.", host, desiredPorts, openPorts)
 
 	if conf.SendEmail == true {
 		auth := smtp.PlainAuth("", conf.SMTPUsername, conf.SMTPPassword, conf.SMTPServer)
